a394ee226257637ab1040a0e5470baf74c350f68
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2021-12-02 11:42:23 -0300

    ci: add typescript settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    semi: 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'import/extensions': [
