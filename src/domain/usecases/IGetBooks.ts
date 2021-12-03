a51a19f4012cd04312f9fff20e198626778faf6d
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2021-12-03 20:08:52 -0300

    fix: remove unused BookFilter

--- src/domain/usecases/IGetBooks.ts
+++ src/domain/usecases/IGetBooks.ts
@@ -1,6 +1,5 @@
 import { Book } from '../models/Book';
-import { BookFilter } from '../models/BookFilter';
 
 export default interface IGetBook {
-    get(filter: BookFilter[]): Promise<Book[]>
+    get(filter: string[]): Promise<Book[]>
 }
