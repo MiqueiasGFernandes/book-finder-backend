fa5f71cb820d5a5d94403de5953cc622cfd8eba7
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2021-12-03 20:01:29 -0300

    ci: add Jest environment configuration

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   testMatch: [
     '<rootDir>/src/**/*.test.ts',
   ],
+  collectCoverageFrom: [
+    '<rootDir>/src/domain/data/**/.ts',
+  ],
+  transform: { '\\.ts$': ['ts-jest'] },
+  setupFiles: ['<rootDir>/scripts/loadReflectMetadata.ts'],
 };
