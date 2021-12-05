591c615ef12c12d7bf8dbb6f603d586870e73a16
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2021-12-05 15:00:27 -0300

    fix: register BookRepository and BookController at DiContainer

--- src/infra/di/DiContainer.ts
+++ src/infra/di/DiContainer.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 
 import { container } from 'tsyringe';
+import BookRepository from '../../data/repositories/BookRepository';
 import GetBook from '../../data/usecases/GetBooks';
 import HttpGoogleApi from '../api/HttpGoogleApi';
 import { EnvironmentConfig } from '../config/EnvironmentConfig';
+import BooksController from '../server/http/controllers/BooksController';
 
 export default class DiContainer {
   static execute() {
@@ -11,5 +13,7 @@ export default class DiContainer {
       .register('IConfig', { useClass: EnvironmentConfig })
       .register('IBookApi', { useClass: HttpGoogleApi })
       .register('IGetBook', { useClass: GetBook })
+      .register('BookRepository', { useClass: BookRepository })
+      .register('BooksController', { useClass: BooksController })
   }
 }
